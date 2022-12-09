dfddb4b78831de92b3bedf9a4c998e9179df4a3d
ShowierData9978 <mellfang36@gmail.com>
2022-12-08 19:21:38 -0600

    revertin this, just a normal cl server

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -2,7 +2,7 @@
  * @file Server-related URL constants.
  */
 
-//export const linkUrl = localStorage.getItem("meower_linkurl") || "wss://api.meower.org/v0/cloudlink/";
-export const linkUrl = "wss://Cloudlink.showierdata9971.repl.co/";
+export const linkUrl = localStorage.getItem("meower_linkurl") || "wss://api.meower.org/v0/cloudlink/";
+
 export const apiUrl = localStorage.getItem("meower_apiurl") || "https://api.meower.org/";
 export const encodeApiURLParams = localStorage.getItem("meower_encodeapi") || false;
