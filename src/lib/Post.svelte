e9f2a20d0c40c5f861cad5e5e30586be8b1d9b28
ShowierData9978 <mellfang36@gmail.com>
2024-01-06 02:12:10 -0600

    Make it slightly more IRC like

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -291,8 +291,298 @@
 		webhook;
 </script>
 
+{#if !tinyTheme}
 <Container>
-	<div class={tinyTheme ? "small" : ''}>
+	<div class="post-header">
+		{#if buttons}
+			<div class="settings-controls">
+				{#if adminView && hasPermission(adminPermissions.DELETE_POSTS)}
+					{#if post.isDeleted}
+						<button
+							class="circle restore"
+							title="Restore post"
+							bind:this={adminRestoreButton}
+							on:click={adminRestore}
+						/>
+					{:else}
+						<button
+							class="circle trash"
+							title="Delete post"
+							bind:this={adminDeleteButton}
+							on:click={adminDelete}
+						/>
+					{/if}
+				{:else if !adminView}
+					{#if !editing && hasPermission(adminPermissions.VIEW_POSTS)}
+						<button
+							class="circle admin"
+							on:click={() =>
+								modals.showModal(ModeratePostModal, {
+									postid: post.post_id,
+								})}
+						/>
+					{/if}
+					{#if input && !input.disabled && !noPFP && !editing}
+						{#if post.user === $user.name}
+							<button
+								class="circle pen"
+								on:click={async () => {
+									editError = "";
+									editing = true;
+									await tick();
+									editContentInput.value =
+										post.unfiltered_content || post.content;
+									editContentInput.focus();
+									autoresize(editContentInput);
+								}}
+							/>
+						{/if}
+						<button
+							class="circle reply"
+							on:click={() => {
+								let existingText = input.value;
+
+								const mentionRegex = /^@\w+\s*/i;
+								const mention = "@" + post.user + " ";
+
+								if (mentionRegex.test(existingText)) {
+									input.value = existingText
+										.trim()
+										.replace(mentionRegex, mention);
+								} else {
+									input.value = mention + existingText.trim();
+								}
+
+								input.focus();
+							}}
+						/>
+						{#if post.user === $user.name || (post.post_origin === $chat._id && $chat.owner === $user.name)}
+							<button
+								class="circle trash"
+								bind:this={deleteButton}
+								on:click={async () => {
+									if (shiftHeld) {
+										deleteButton.disabled = true;
+										try {
+											const resp = await fetch(
+												`${apiUrl}posts?id=${post.post_id}`,
+												{
+													method: "DELETE",
+													headers: $authHeader,
+												}
+											);
+											if (!resp.ok) {
+												if (resp.status === 429) {
+													throw new Error(
+														"Too many requests! Try again later."
+													);
+												}
+												throw new Error(
+													"Response code is not OK; code is " +
+														resp.status
+												);
+											}
+										} catch (e) {
+											editError = e;
+										}
+										deleteButton.disabled = false;
+									} else {
+										modals.showModal(DeletePostModal, {
+											post,
+										});
+									}
+								}}
+							/>
+						{:else}
+							<button
+								class="circle report"
+								on:click={() =>
+									modals.showModal(ReportPostModal, {post})}
+							/>
+						{/if}
+					{/if}
+				{/if}
+			</div>
+		{/if}
+		<button
+			class="pfp"
+			on:click={async () => {
+				if (noPFP) return;
+				$goto(`/users/${post.user}`);
+			}}
+		>
+			{#await noPFP ? Promise.resolve(true) : loadProfile(post.user)}
+				<PFP
+					icon={-2}
+					alt="{post.user}'s profile picture"
+					online={$ulist.includes(post.user)}
+				/>
+			{:then profile}
+				<PFP
+					icon={noPFP
+						? post.user === "Server"
+							? 102
+							: post.post_origin === "inbox" &&
+							  (post.user === "Announcement" ||
+									post.user === "Notification" ||
+									post.user.startsWith("Notification to"))
+							? 101
+							: -2
+						: profile.pfp_data}
+					alt="{post.user}'s profile picture"
+					online={$ulist.includes(post.user)}
+				/>
+			{:catch}
+				<PFP
+					icon={-2}
+					tinyTheme={tinyTheme}
+					alt="{post.user}'s profile picture"
+					online={$ulist.includes(post.user)}
+				/>
+			{/await}
+		</button>
+		<div class="creatordate">
+			<div class="creator">
+				<h2>
+					<LiText text={post.user} />
+				</h2>
+
+				{#if bridged}
+					<Badge
+						text="BRIDGED"
+						title="This post is bridged from an external service by a bot"
+					/>
+				{/if}
+
+				{#if webhook}
+					<Badge
+						text="WEBHOOK"
+						title="This post was posted by the @Webhooks bot. The username may not mean the user actually posted it!"
+					/>
+				{/if}
+
+				<!-- disabled until proper bot badges are added
+				{#if post.isvbot && !webhook}
+					<Badge
+						text="BOT"
+						title="This bot has been verified"
+						checkmark={true}
+					/>
+				{/if}
+
+				{#if post.isuvbot && !webhook}
+					<Badge text="BOT" title="This bot has not been verified" />
+				{/if}
+				-->
+			</div>
+
+			<FormattedDate date={post.date} />
+			{#if post.edited_at}
+				<i
+					>(<FormattedDate
+						date={post.edited_at}
+						customText="edited"
+					/>)</i
+				>
+			{/if}
+			{#if post.isDeleted && post.mod_deleted}
+				<i
+					>(<FormattedDate
+						date={post.deleted_at}
+						customText="deleted by moderator"
+					/>)</i
+				>
+			{:else if post.isDeleted}
+				<i
+					>(<FormattedDate
+						date={post.deleted_at}
+						customText="self-deleted"
+					/>)</i
+				>
+			{/if}
+		</div>
+	</div>
+	{#if editing}
+		<textarea
+			type="text"
+			class="white"
+			name="input"
+			autocomplete="off"
+			maxlength="4000"
+			rows="1"
+			bind:this={editContentInput}
+			use:autoresize
+			on:keydown={event => {
+				if (event.key == "Enter" && !shiftHeld) {
+					event.preventDefault();
+					if (!editSaveButton.disabled) editSaveButton.click();
+				} else if (event.key == "Escape") {
+					editing = false;
+				}
+			}}
+		/>
+		<div style="display: flex; justify-content: space-between;">
+			<button on:click={() => (editing = false)}>Cancel</button>
+			<button
+				bind:this={editSaveButton}
+				on:click={async () => {
+					if (editContentInput.value.trim() === "") {
+						modals.showModal(DeletePostModal, {post});
+						return;
+					}
+
+					editing = false;
+					try {
+						const resp = await fetch(
+							`${apiUrl}posts?id=${post.post_id}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									...$authHeader,
+								},
+								body: JSON.stringify({
+									content: editContentInput.value,
+								}),
+							}
+						);
+						if (!resp.ok) {
+							if (resp.status === 429) {
+								throw new Error(
+									"Too many requests! Try again later."
+								);
+							}
+							throw new Error(
+								"Response code is not OK; code is " +
+									resp.status
+							);
+						}
+					} catch (e) {
+						editError = e;
+					}
+				}}>Save</button
+			>
+		</div>
+	{:else}
+		<p class="post-content" style="border-left-color: #4b5563;">
+				{#await addFancyElements(post.content) then content}
+					{@html content}
+				{/await}
+		</p>
+	{/if}
+	{#if editError}
+		<p style="color: crimson;">{editError}</p>
+	{/if}
+	{#if !editing}
+		<div class="post-images">
+			{#each images as { title, url }}
+				<Attachment {title} {url} />
+			{/each}
+		</div>
+	{/if}
+</Container>
+{:else}
+<div class="small">
 	<div class="post-header">
 		{#if buttons}
 			<div class="settings-controls">
@@ -567,30 +857,16 @@
 		</div>
 	{:else}
 		<p class="post-content" style="border-left-color: #4b5563;">
-			{#if (!tinyTheme)}
-				{#await addFancyElements(post.content) then content}
-					{@html content}
-				{/await}
-			{:else}
-				{post.content}
-			{/if}
+			{post.content}
 		</p>
 	{/if}
 	{#if editError}
 		<p style="color: crimson;">{editError}</p>
 	{/if}
-	{#if !editing}
-		{#if !tinyTheme}
-		<div class="post-images">
-			{#each images as { title, url }}
-				<Attachment {title} {url} />
-			{/each}
-		</div>
-		{/if}
-	{/if}
 	</div>
 
-</Container>
+
+{/if}
 
 <style>
 	.pfp {
@@ -604,8 +880,9 @@
 	.small {
 		display: inline-flex !important;
 		max-height: 11% !important;
-		padding: 1px !important;
-		margin: 0px !important;
+		padding: 0px !important;
+		margin-top: -10px !important;
+		margin-bottom: -10px !important;
 		white-space: nowrap !important;
 		align-items: center !important;
 	}
@@ -638,6 +915,9 @@
 		position: absolute !important;
 		top: 25%;
 		right: 0.25em;
+		margin: 0px !important;
+		padding: 0px !important;
+		z-index: 1000;
 	}
 
 
@@ -678,6 +958,11 @@
 		border: none;
 		margin: 0;
 		margin-left: 0.125em;
+	}
+
+	.tiny * button.circle {
+		margin-top: 0px !important;
+		margin-bottom: 0px !important;
 	}
 
 	.post-images {
